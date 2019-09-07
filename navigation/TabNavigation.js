0c5d3725519d0e4e6ae219e8e8f1bd23e89e0974
프렛의 브이로그 <fredkeemhaus@gmail.com>
2019-09-07 22:30:52 +0900

    Custom Icons for the Tab Navigation

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,3 +1,5 @@
+import React from 'react'
+import { Platform } from 'react-native'
 import { createAppContainer } from 'react-navigation'
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 
@@ -6,11 +8,18 @@ import MoviesScreen from '../screens/Movies'
 import SearchScreen from '../screens/Search'
 
 import { BG_COLOR } from '../constants/Colors'
+import TabBarIcon from '../components/TabBarIcon';
 
 const TabNavigation = createBottomTabNavigator({
-    Movies: MoviesScreen,
-    TV: TVScreen,
-    Search: SearchScreen
+    Movie: {screen: MoviesScreen, navigationOptions: {
+      tabBarIcon: ({focused}) => <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-film': 'md-film'}/>
+    }},
+    TV: {screen: TVScreen, navigationOptions: {
+      tabBarIcon: ({focused}) => <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-tv': 'md-tv'}/>
+    }},
+    Search: {screen: SearchScreen, navigationOptions: {
+      tabBarIcon: ({focused}) => <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-search': 'md-search'}/>
+    }}
   }, {
     tabBarOptions: {
       showLabel: false,
