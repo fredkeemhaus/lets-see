5e6ee4057e6aadcbcd6ac64de9502fb35c325f2b
프렛의 브이로그 <fredkeemhaus@gmail.com>
2019-09-07 22:14:16 +0900

    Set Tab Navigation

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import { AppLoading } from 'expo'
+import * as Font from 'expo-font'
+import {Ionicons} from '@expo/vector-icons'
 import { StyleSheet, Text, View } from 'react-native';
+import TabNavigation from './navigation/TabNavigation'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    loaded: false
+  }
+
+  handleError = (error) => { console.log(error) }
+
+  handleLoaded = () => this.setState({loaded: true})
+
+  loadAssets = async () => {
+    await Font.loadAsync({
+      ...Ionicons.font
+    })
+  }
+
+  render() {
+    const { loaded } = this.state
+    if (loaded) {
+      return (
+      // <View style={styles.container}>
+        <TabNavigation />
+      // </View>
+    )
+    } else {
+      return <AppLoading startAsync={this.loadAssets} onFinish={this.handleLoaded} onError={this.handleError}/>
+    }
+  }
 }
 
 const styles = StyleSheet.create({
---
+++ README.md
@@ -0,0 +1,10 @@
+# Let see
+
+IOS / Android Movie Discovery App built with React Native.
+
+## Screens
+
+- [ ] Movies
+- [ ] TV Shows
+- [ ] Search
+- [ ] Detail
