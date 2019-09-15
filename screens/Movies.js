e39030c30630e34f4a14c98290c3c0149b45afab
프렛의 브이로그 <fredkeemhaus@gmail.com>
2019-09-15 16:06:17 +0900

    Home Container

--- screens/Movies.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Text, TouchableOpacity } from 'react-native'
-
-export default ({navigation}) =>  (
-<TouchableOpacity onPress={() => navigation.navigate('Detail')} style={{marginTop: 200}}>
-  <Text>Go to Detail</Text>
-  </TouchableOpacity>
-  )
