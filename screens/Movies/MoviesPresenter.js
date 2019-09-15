ca03393ea09957026430f4cc55dd0ac3d5a5f821
프렛의 브이로그 <fredkeemhaus@gmail.com>
2019-09-15 16:21:15 +0900

    Loading Component

--- screens/Movies/MoviesPresenter.js
+++ screens/Movies/MoviesPresenter.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Text } from 'react-native'
 import PropTypes from 'prop-types'
+import Loader from '../../components/Loader';
 
-const MoviesPresenter = () => <Text>Movies</Text>
+const MoviesPresenter = ({loading}) => loading ? <Loader /> : <Text>Movies</Text>
 
 MoviesPresenter.propTypes = {
   loading: PropTypes.bool.isRequired
